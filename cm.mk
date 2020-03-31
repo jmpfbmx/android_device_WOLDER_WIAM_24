8e2d75cf88a486cda88c7836e2b731b500764282
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-03-31 13:08:23 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,19 +1,19 @@
 # Release name
-PRODUCT_RELEASE_NAME := i504
+PRODUCT_RELEASE_NAME := zeus
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/nomi/i504/device_i504.mk)
+$(call inherit-product, device/wolder/zeus/device_zeus.mk)
 
 # Correct bootanimation size for the screen
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_i504
-PRODUCT_DEVICE :=i504
-PRODUCT_BRAND := nomi
-PRODUCT_MANUFACTURER := Nomi
-PRODUCT_MODEL := i504 Dream
+PRODUCT_NAME := cm_zeus
+PRODUCT_DEVICE := zeus
+PRODUCT_BRAND := Wolder
+PRODUCT_MANUFACTURER := Wolder
+PRODUCT_MODEL := WIAM #24
